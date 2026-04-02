e127028dcaa01eb9cf72ad1a2b9ebecb2f4ab62b
毛利哲也 <mouritetsuya@mouritetsuyanoMacBook-Pro.local>
2026-04-02 19:41:42 +0900

    fix: Viteの動的ベースパス設定とルート相対パスへの完全復旧

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: process.env.NODE_ENV === 'production' ? '/puzzle-inc-site/' : '/',
   appType: 'mpa',
   plugins: [react()],
   build: {
