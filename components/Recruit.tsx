c40be6cc8598a1dfead82e0257105ba80ad15a4d
毛利哲也 <mouritetsuya@mouritetsuyanoMacBook-Pro.local>
2026-04-06 17:10:43 +0900

    Remove abstract background shape from Recruit section to clean up the UI.

--- components/Recruit.tsx
+++ components/Recruit.tsx
@@ -7,12 +7,6 @@ import TextReveal from './TextReveal';
 const Recruit: React.FC = () => {
   return (
     <section id="recruit" className="relative py-40 bg-gray-900 text-white overflow-hidden">
-        {/* Abstract shape background */}
-        <div className="absolute top-0 right-0 w-1/2 h-full opacity-10 pointer-events-none">
-            <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" className="w-full h-full animate-pulse-slow">
-                <path fill="#FFFFFF" d="M42.7,-62.9C50.9,-52.8,50.1,-34.4,51.7,-19.2C53.4,-4,57.4,8,54.5,19.1C51.6,30.2,41.8,40.4,30.3,48.8C18.8,57.1,5.6,63.6,-6.4,62.7C-18.4,61.8,-29.2,53.5,-40.4,45.2C-51.6,36.9,-63.2,28.6,-68.8,16.7C-74.4,4.8,-74,-10.7,-66.2,-23.1C-58.5,-35.5,-43.3,-44.8,-29.8,-53.2C-16.3,-61.6,-4.5,-69.2,6.4,-68.1C17.3,-67,34.5,-73,42.7,-62.9Z" transform="translate(100 100)" />
-            </svg>
-        </div>
         
         {/* Marquee Background */}
         <div className="absolute top-1/2 left-0 w-full transform -translate-y-1/2 -rotate-3 opacity-5 pointer-events-none select-none">
